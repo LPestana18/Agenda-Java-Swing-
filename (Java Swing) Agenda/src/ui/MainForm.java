a63bcf2e98e08769c51c90777ad3a8dbadd15fe2
LPestana18 <lucaspestana0212@gmail.com>
2020-08-11 17:36:55 -0300

    corpo do projeto

---
+++ (Java Swing) Agenda/src/ui/MainForm.java
@@ -0,0 +1,23 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainForm extends JFrame {
+    private JPanel rootPanel;
+    private JButton buttonNewContact;
+    private JButton buttonRemove;
+    private JTable tableContacts;
+
+    public MainForm() {
+
+        setContentPane(rootPane);
+        setSize(500, 250);
+        setVisible(true);
+
+        Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+        setLocation(dim.width / 2 - getSize().width, dim.height / 2 - getSize().height / 2);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+}
